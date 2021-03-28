7d4ddc3c6258270acfb46b95eb85b68a13f5448b
Murgiuf <murgiuf@gmail.com>
2021-03-28 10:40:26 -0500

    commit inicial

---
+++ app/models/contacto.rb
@@ -0,0 +1,24 @@
+class Contacto < ApplicationRecord
+  belongs_to :carga
+
+  def desencripta_tarjeta
+    tarjeta = self.tarjeta
+
+    if tarjeta.blank?
+      return
+    end
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base[0..31])
+    decrypted_back = crypt.decrypt_and_verify(tarjeta)
+    return decrypted_back.split(//).last(4).join
+
+  rescue
+    "invalido"
+  end
+
+  def self.encripta_tarjeta(tarjeta)
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base[0..31])
+    encrypted_data = crypt.encrypt_and_sign(tarjeta)
+  end
+
+end
